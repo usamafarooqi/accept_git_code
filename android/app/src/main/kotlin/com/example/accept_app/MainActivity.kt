7f81249bac7253ed4c75d5602ddf4dda575c59c1
DESKTOP-RE7MPC7\Securehops <securehops@gmail.com>
2021-09-27 07:12:54 -0700

    accept demo from git

---
+++ android/app/src/main/kotlin/com/example/accept_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.accept_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color backgroundColor0 =  Color(0xffFCD5C7);
+const Color backgroundColor1 = Color(0xffE5EBED);
+const Color backgroundColor2 = Color(0xffDCD1EF);
+
+const Color whiteColor = Color(0xffFBFCFD) ;
+const Color blackColor = Color(0xff252D48);
+const Color userShadow = Color(0xff12637D);
+
+const Color partColor0 = Color(0xffF4DBE6);
+const Color partColor1 = Color(0xffFFB987);
+const Color partColor2 = Color(0xffFF78A9);
+const Color partColor3 = Color(0xffDA8AFF);
+
+TextStyle UserStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w700,
+  fontSize: 40,
+  color: blackColor,
+);
+
+TextStyle TopicStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w700,
+  fontSize: 30,
+);
+
+
+TextStyle UserTopicStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w700,
+  fontSize: 20,
+);
+
+TextStyle UserSubtitleStyle = GoogleFonts.poppins(
+  fontSize: 18,
+  color: blackColor.withOpacity(0.5),
+);
+
+TextStyle UserOptionsStyle = GoogleFonts.poppins(
+  fontSize: 16,
+  color: blackColor,
+);
